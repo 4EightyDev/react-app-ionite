1d51e9f4955e3ed346a406728d0fddfd69f60e2c
Tom Swain <113206264+4EightyDev@users.noreply.github.com>
2022-11-04 07:36:58 -0700

    edit btn

--- src/components/modules/BtnIonite.jsx
+++ src/components/modules/BtnIonite.jsx
@@ -9,7 +9,7 @@ const BtnPrimary = (props) => {
 			className={`group rounded-full shadow-lg shadow-sky-500/10 ${props.className}`}>
 			<div class='flex rounded-full bg-gradient-to-r from-sky-500/20 via-green-300 to-sky-500/20 p-px'>
 				<div class='flex-center relative min-w-[200px] rounded-full bg-slate-900 px-5 py-3'>
-					<span className='text-lg font-bold leading-tight text-sky-200 transition-all group-hover:text-white'>
+					<span className='text-lg font-bold leading-tight text-sky-200 transition-all group-hover:text-sky-100'>
 						{props.text}
 					</span>
 					<div class='max-w-3/4 absolute top-[-1px] left-12 right-12 h-[2px] bg-gradient-to-r from-green-400/0 via-green-400 to-green-400/0 transition-all group-hover:left-6 group-hover:right-6 group-hover:top-[-2px] group-hover:h-[3px]'></div>
